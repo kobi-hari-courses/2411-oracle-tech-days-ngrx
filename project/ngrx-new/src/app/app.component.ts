550289f3bb2b4c68ab303274dc5bdf69ef9d608e
Kobi Hari <kobi2294@yahoo.com>
2024-11-22 17:48:01 +0200

    migration completed

--- project/ngrx-new/src/app/app.component.ts
+++ project/ngrx-new/src/app/app.component.ts
@@ -1,11 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SharedModule } from './shared.module';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { QuestionPresenterComponent } from './components/question-presenter/question-presenter.component';
-import { Question } from './models/question.model';
 import { ProgressComponent } from './components/progress/progress.component';
 import { DoneComponent } from './components/done/done.component';
-import { randomColorQuestion } from './services/helpers';
 import { QuizStore } from './store/quiz.store';
 
 @Component({
